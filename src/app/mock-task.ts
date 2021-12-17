8a71d0daa258be4c4253bf304ca8f90b169e798e
Vaider13 <pabloquirco@hotmail.com>
2021-12-17 11:51:09 -0300

    lista de tareas

--- src/app/mock-task.ts
+++ src/app/mock-task.ts
@@ -2,7 +2,7 @@ import {Task} from "./task";
 export const TASKS:Task[] = [
   {
     id: 1,
-    text:"terminar primer modulo de angular",
+    text:"Terminar primer modulo de angular",
     day: "13 de Diciembre a las 16:00",
     reminder: true
   },

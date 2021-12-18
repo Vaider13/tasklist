3fb5d1e54c474bb1d05848bb1a15300c5630d13d
Vaider13 <pabloquirco@hotmail.com>
2021-12-17 22:10:22 -0300

    Basta por hoy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskService } from '../../service/task.service';
 import {Task} from '../../task'
 import {TASKS} from '../../mock-task'
+
+
 
 @Component({
   selector: 'app-tasks',
@@ -8,10 +11,14 @@ import {TASKS} from '../../mock-task'
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] =TASKS;
+  tasks: Task[]=[];
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks
+    });
   }
-
 }
